8b7245642dc8fd11e1e5227eab86ecde672dfc33
Edward Mara <e.james.mara@gmail.com>
2019-05-30 16:26:06 -0700

    potential word array and code for random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,17 @@
-// <!-- TODO: create array with words to play -->
-//         <!-- randomly choose a word -->
-//         <!-- create an array of blank spaces to represent the word to be guessed. -->
-//         <!-- initialize variable for remainingLetters to update throughout -->
+//         <!--TODO: create an array of blank spaces to represent the word to be guessed. -->
+//         <!--TODO: initialize variable for remainingLetters to update throughout -->
 
-    
+// <!-- create array with words to play -->
+var wordArray = ["ball", "strike", "hit", "catch", "throw", "mitt", "homerun", "single", "double", "triple", "catcher", "umpire", "pitch", "pitcher", "base", "bunt", "mound"];
+
+//         <!-- randomly choose a word -->
+var word = wordArray[Math.floor(Math.random() * words.length)];
+
+
+
+
+
+
 //     <!-- TODO: take user input and process through word -->
 //         <!--for (length of word)
 //                 if(guessed letter === letter in word)
@@ -12,4 +20,4 @@
 //     <!-- TODO: update and display wins and losses-->
 
 
-var wordArray = ["ball"]
+
